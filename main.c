a80a2697cc38ad33ca5e3ccccfb22e0e19ee1267
Shuyang Li <shuyang.li.95@gmail.com>
2013-11-20 22:09:05 -0500

    more implementation on start_game, and more game logic

--- main.c
+++ main.c
@@ -34,16 +34,19 @@ typedef enum { false = 0, true } boolean;
 void open_graphics();
 
 // show game menu and get user input
-void show_game_menu(int * usr);
-
-// sets up a new game of sudoku, and returns when the user quits
-void start_game();
+void show_game_menu(int *userChoice);
 
 // displays instructions, and returns as the user prompts
 void show_instructions();
 
 // generates the solution board, and removes an appropriate number of numbers to generate the puzzle board, and saves the valid positions in the valid positions board
-void generateBoard(int *solutionBoard[BOARD_SIZE][BOARD_SIZE], int *puzzleBoard[BOARD_SIZE][BOARD_SIZE], boolean *validPositions[BOARD_SIZE][BOARD_SIZE]);
+void generateBoard(int solutionBoard[BOARD_SIZE][BOARD_SIZE], int puzzleBoard[BOARD_SIZE][BOARD_SIZE], boolean validPositions[BOARD_SIZE][BOARD_SIZE]);
+
+// helper function to translate from screen location to board index
+void screenToIndex(const int screenX, const int screenY, int *indexX, int *indexY);
+
+// helper function to translate from board index to screen location
+void indexToScreen(const int indexX, const int indexY, int *screenX, int *screenY);
 
 /*
    ==========================================
@@ -51,11 +54,17 @@ void generateBoard(int *solutionBoard[BOARD_SIZE][BOARD_SIZE], int *puzzleBoard[
    ==========================================
 */
 
+// sets up a new game of sudoku, and returns when the current game ends
+void start_game();
+
 // given a position (x, y) on the board, this functions returns if the position is available for the user to change, i.e. it's not one of the computer-generated numbers
 boolean isValidPosition(const int posX, const int posY, const char validPositions[BOARD_SIZE][BOARD_SIZE]);
 
 // checks if the game ends
 boolean isGameEnd(const int puzzleBoard[BOARD_SIZE][BOARD_SIZE]);
+
+// updates the board with a given input at a given position
+void updateBoard(const int xPos, const int yPos, const int inputNum, int puzzleBoard[BOARD_SIZE][BOARD_SIZE]);
 
 /*
    =============
@@ -95,15 +104,11 @@ int main() {
 				userQuitsGame = true;
 				break;
 		}
-
 	}
 	
 	return 0;
 }
 
-boolean isValidPosition(const int posX, const int posY, const char validPositions[BOARD_SIZE][BOARD_SIZE]) {
-	return validPositions[posX][posY];
-}
 
 void start_game() {
 
@@ -111,15 +116,32 @@ void start_game() {
 	int solutionBoard[BOARD_SIZE][BOARD_SIZE] = { 0 };
 	int puzzleBoard[BOARD_SIZE][BOARD_SIZE] = { 0 };
 	boolean validPositions[BOARD_SIZE][BOARD_SIZE] = { 0 };
+	boolean userGivesUp = false;
 
-	// to be completed
-	generateBoard(&solutionBoard, &puzzleBoard, &validPositions);
+	generateBoard(solutionBoard, puzzleBoard, validPositions);
 
+	while (!isGameEnd(puzzleBoard) && !userGivesUp) {
+		print_board(puzzleBoard);
+
+		int xPos, yPos, inputNum;
+		get_user_input(&xPos, &yPos, &inputNum);
+
+		if (isValidPosition(xPos, yPos, validPositions) && isValidMove(xPos, yPos, inputNum, puzzleBoard)) {
+			updateBoard(xPos, yPos, inputNum, puzzleBoard);
+		} else {
+			promptInvalid(xPos, yPos, inputNum);
+		}
+	}
+
+	if (userGivesUp) {
+		print_board(solutionBoard);
+		// wait for user to continue
+	}
 }
 
-void generateBoard(int *solutionBoard[BOARD_SIZE][BOARD_SIZE], int *puzzleBoard[BOARD_SIZE][BOARD_SIZE], boolean *validPositions[BOARD_SIZE][BOARD_SIZE]) {
+void generateBoard(int solutionBoard[BOARD_SIZE][BOARD_SIZE], int puzzleBoard[BOARD_SIZE][BOARD_SIZE], boolean validPositions[BOARD_SIZE][BOARD_SIZE]) {
 
-	// to be completed
+	// to be implemented
 }
 
 boolean isGameEnd(const int puzzleBoard[BOARD_SIZE][BOARD_SIZE]) {
@@ -133,4 +155,41 @@ boolean isGameEnd(const int puzzleBoard[BOARD_SIZE][BOARD_SIZE]) {
 	}
 
 	return (numBlank == 0);
+}
+
+void print_board(const int puzzleBoard[BOARD_SIZE][BOARD_SIZE]) {
+	
+	// to be implemented
+}
+
+void get_user_input(int *xPos, int *yPos) {
+
+	// to be implemented
+}
+
+boolean isValidPosition(const int posX, const int posY, const char validPositions[BOARD_SIZE][BOARD_SIZE]) {
+
+	return validPositions[posX][posY];
+}
+
+boolean isValidMove(const int xPos, const int yPos, const int inputNum, const int puzzleBoard) {
+
+	// to be implemented
+}
+
+void updateBoard(const int xPos, const int yPos, const int inputNum, int puzzleBoard[BOARD_SIZE][BOARD_SIZE]) {
+	// it's okay to just set the board, since we have checked that the input is valid
+	puzzleBoard[xPos][yPos] = inputNum;
+}
+
+void promptInvalid(const int xPos, const int yPos, const int inputNum) {
+
+}
+
+void screenToIndex(const int screenX, const int screenY, int *indexX, int *indexY) {
+
+}
+
+void indexToScreen(const int indexX, const int indexY, int *screenX, int *screenY) {
+
 }
