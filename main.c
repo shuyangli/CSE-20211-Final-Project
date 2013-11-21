0e1587aa1e22a97a944bd81703a0cbb8b2d22207
Shuyang Li <shuyang.li.95@gmail.com>
2013-11-20 20:57:21 -0500

    added info notation at the beginning

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/** 
+ *	main.c
+ *	This program is a sudoku game that generates a random sudoku board every time,
+ *	and provides solution to the user if the user cannot figure it out.
+ *
+ *	Shuyang Li and Adam Goins
+ *	for CSE 20211
+ *	First written on November 20, 2013
+ **/
+
 #include <stdio.h>
 #include "gfx.h"
 
